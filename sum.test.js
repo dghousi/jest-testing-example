b0c9ce6dcc876b037b4440b645c69b482ceb0fb8
Dawlatzai <dawlatzai.ghousi@gmail.com>
2022-07-14 11:06:07 +0430

    add toBeGreaterThan and toBeLessThan expect methods

--- sum.test.js
+++ sum.test.js
@@ -30,6 +30,8 @@ describe('numbers', () => {
   it('two plus two', () => {
     const value = 2 + 2
     expect(value).toBe(4)
+    expect(value).toBeGreaterThan(2)
+    expect(value).toBeLessThan(5)
   })
 
   it('adding floats ', () => {
